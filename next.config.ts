d6663409e1689f8c4efe063d6f7608ae7cccfa53
Danny Rodriguez <eyedeeaa@gmail.com>
2025-11-12 20:24:29 -0800

    feat: add github pages deployment workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/portfolio-website",
+  output: "export",
 };
 
 export default nextConfig;
+
